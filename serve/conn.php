20fe1fc023b816cc21f714e6a74f7fbb286abca9
Tracy Silverio <tiritord@gmail.com>
2023-08-16 12:15:46 -0400

    google

---
+++ serve/conn.php
@@ -0,0 +1,12 @@
+<!-- <?php
+
+
+$conection = mysqli_connect('localhost', 'root', '', 'hacker');
+if($conection){
+    echo "Conexion exitosa";
+
+}else{
+     echo 'conexion mala';
+}
+
+    ?> -->
